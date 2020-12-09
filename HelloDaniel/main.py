90b4ada483785ec8a234cecb092025890cdd21e0
danielbuaron <75724440+danielbuaron@users.noreply.github.com>
2020-12-09 11:12:32 +0200

    Add files via upload

---
+++ HelloDaniel/main.py
@@ -0,0 +1,23 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name, x=100):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {x} {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+def print_hello(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'hello, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hello('daniel')
+    print_hi('alon')
+    print_hi('michal', 1000)
+    print_hi('סימה',50)
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
